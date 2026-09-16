bdd55dca5ac0735235b55abd2a50e9d79b573c87
Ajeet Jain <ajeetjain9958@gmail.com>
2026-09-16 04:10:51 +0000

    container config done

---
+++ .devcontainer/01_variables.js
@@ -0,0 +1,11 @@
+const accountid=234059
+let accountEmail="ajeet@goggle.com"
+var accountpass="23333"
+accountcity="jaipur"
+ //accountid=2 //not allowed
+ //const can be changed after first decalare in the program
+
+ accountEmail="ajeettt@goggle.com"
+accountpass="2337733"
+accountcity="udaipur"
+console.log([accountEmail,accountpass,accountcity,accountid])
